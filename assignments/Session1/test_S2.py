ac42ac98029ad2d5898f1b057f9390dff64ae32d
EvanRicchi <e.ricchi@hotmail.fr>
2017-10-03 12:02:57 +0200

    Correction of bugs and random_fill_sparse test

--- assignments/Session1/test_S2.py
+++ assignments/Session1/test_S2.py
@@ -38,6 +38,11 @@ def test_average_above_zero():
 def test_max_value():
     ##
     # Basic function able to test max_value function
+
+    # Testing with an empty list
+    test_list=[]
+    with pytest.raises(ValueError):
+        algo.max_value(test_list)
     
     # Testing with a random classic list
     test_list=[1,2,3,4,-7]
@@ -59,9 +64,16 @@ def test_reverse_table():
     ##
     # Basic function able to test reverse_table function
     
+    # Testing with an empty list
+    test_list=[]
+    with pytest.raises(ValueError):
+        algo.reverse_table(test_list)
+    
     # Initialise test list   
     test_list=[1,2,3,4,-7]
     test_reversed_list=[-7,4,3,2,1]
+    
+    # Testing with a basic list
     assert algo.reverse_table(test_list) == test_reversed_list
     
 def test_roi_bbox():
@@ -81,7 +93,12 @@ def test_roi_bbox():
 def test_shuffle():
     ##
     # Basic function able to test shuffle function
-
+    
+    # Testing with an empty list
+    test_list=[]
+    with pytest.raises(ValueError):
+        algo.shuffle(test_list)
+        
     # Initialise test list    
     test_list=[1,2,3]
     # Test all the possibilities with a shuffle
@@ -90,7 +107,12 @@ def test_shuffle():
 def test_remove_whitespace():
     ##
     # Basic function able to test remove_whitespace function
-
+    
+    # Testing with an empty string
+    test_string=""
+    with pytest.raises(ValueError):
+        algo.remove_whitespace(test_string)
+        
     # Initialise test string    
     test_string="This is a test 1 2 3"
     # Test with a ordinary string
@@ -105,12 +127,17 @@ def test_sort_selective():
     ##
     # Basic function able to test sort_selective function
 
+    # Testing with an empty list
+    test_list=[]
+    with pytest.raises(ValueError):
+        algo.sort_selective(test_list)
+    
     # Initialise test list    
     test_list=[10, 15, 7, 1, 3, 3, 9]
     # Test with a ordinary list
     assert algo.sort_selective(test_list) == [1, 3, 3, 7, 9, 10, 15]
     
-    # Initialise test string    
+    # Initialise test list    
     test_string=[-10, -15, -7, -1, -3, -3, -9]
     # Test with a list of negative values
     assert algo.sort_selective(test_string) == [-15, -10, -9, -7, -3, -3, -1]
@@ -119,12 +146,27 @@ def test_sort_bubble():
     ##
     # Basic function able to test sort_bubble function
 
+    # Testing with an empty list
+    test_list=[]
+    with pytest.raises(ValueError):
+        algo.sort_bubble(test_list)
+    
     # Initialise test list    
     test_list=[10, 15, 7, 1, 3, 3, 9]
     # Test with a ordinary list
     assert algo.sort_bubble(test_list) == [1, 3, 3, 7, 9, 10, 15]
     
-    # Initialise test string    
+    # Initialise test list    
     test_string=[-10, -15, -7, -1, -3, -3, -9]
     # Test with a list of negative values
     assert algo.sort_bubble(test_string) == [-15, -10, -9, -7, -3, -3, -1]
+
+def test_random_fill_sparse():
+    ##
+    # Basic function able to test random_fill_sparse function
+
+    input_array=np.chararray((10, 10))
+    input_array[:] = ''
+    vfill= input_array.size
+    alea = algo.random(vfill)
+    result = algo.random_fill_sparse(input_array,alea)
